87a97a8e848f0e24cf8b2294403929ea0bdfbfa3
Southclaws <Southclaws@gmail.com>
2017-07-07 23:55:19 +0100

    implementing the servers endpoint for listing servers in a minimal fashion - i.e. without all the unnecessary fields, only what's needed to build a server list UI

--- servers.go
+++ servers.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"gopkg.in/mgo.v2/bson"
 )
 
 // Servers returns a JSON encoded array of available servers
 func (app *App) Servers(w http.ResponseWriter, r *http.Request) {
 	logger.Debug("getting server list")
+
+	servers := []Core{}
+	err := app.GetServers(&servers)
+	if err != nil {
+		WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(servers)
+	if err != nil {
+		WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+}
+
+// GetServers returns a slice of Core objects
+func (app *App) GetServers(servers *[]Core) (err error) {
+	err = app.db.Find(bson.M{}).All(&servers)
+	return
 }
