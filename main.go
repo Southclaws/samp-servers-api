0f382f4f9c5f47d122268c30885f7c133938fd89
Barnaby <barnaby@spotlightdata.co.uk>
2017-09-12 11:07:41 +0100

    new logging method

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 // Config stores app global configuration
@@ -23,19 +24,32 @@ type Config struct {
 var logger *zap.Logger
 
 func init() {
-	var err error
 	var config zap.Config
+	debug := os.Getenv("DEBUG")
 
-	config = zap.NewProductionConfig()
+	if os.Getenv("TESTING") != "" {
+		config = zap.NewDevelopmentConfig()
+		config.DisableCaller = true
+	} else {
+		config = zap.NewProductionConfig()
+		config.EncoderConfig.MessageKey = "@message"
+		config.EncoderConfig.TimeKey = "@timestamp"
+		config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 
-	// dyn := zap.NewAtomicLevel()
-	// dyn.SetLevel(zap.DebugLevel)
-	// config.Level = dyn
-
-	logger, err = config.Build()
+		if debug != "0" && debug != "" {
+			dyn := zap.NewAtomicLevel()
+			dyn.SetLevel(zap.DebugLevel)
+			config.Level = dyn
+		}
+	}
+	_logger, err := config.Build()
 	if err != nil {
 		panic(err)
 	}
+	logger = _logger.With(
+		zap.String("@version", os.Getenv("GIT_HASH")),
+		zap.Namespace("@fields"),
+	)
 }
 
 func main() {
