f8e3d63d1b25f1364970eb025f47be089775bc73
Southclaws <SouthcawJK@gmail.com>
2017-10-16 08:38:29 +0100

    finished graphql types

--- graphql.go
+++ graphql.go
@@ -4,6 +4,19 @@ import (
 	"net/http"
 )
 
+// QueryRequest represents a GraphQL POST request payload
+type QueryRequest struct {
+	Query     string
+	Operation string
+	Variables map[string]interface{}
+}
+
+// QueryResponse represents a GraphQL response according to the HTTP spec
+type QueryResponse struct {
+	Data   map[string]interface{}
+	Errors []string
+}
+
 // GraphQL handles GraphQL queries and conforms to http://graphql.org/learn/serving-over-http/
 func (app *App) GraphQL(w http.ResponseWriter, r *http.Request) {
 
